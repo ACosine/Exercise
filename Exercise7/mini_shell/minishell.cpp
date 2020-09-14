84c7b1d327bbdb1514e2ead450cc0e29b7ec6bd1
ACosine <1303881850@qq.com>
2020-09-14 22:12:47 +0800

    练习第七天

---
+++ Exercise7/mini_shell/minishell.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+int GetCommand()
+{
+    
+
+}
+
+int DealCommand(char* command)
+{
+    
+
+}
+
+
+int main()
+{
+
+while(1)
+{
+    GetCommand();
+
+
+
+
+}
+
+
+    return 0;
+}
